cfaa1aec2d66bdf9411f25c558f74d85b87fd12f
Viraj Dobariya <virajdobariya24@gmail.com>
2026-07-12 14:58:40 +0530

    update settings1

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,6 +16,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Installed apps
